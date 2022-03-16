027fab8a2777f12baac167578f93ca2b098f9858
Juan Carlos <93095797+JuanCarlosFR26@users.noreply.github.com>
2022-03-16 19:58:11 +0100

    Add files via upload

---
+++ Ejercicio2/app.js
@@ -0,0 +1,9 @@
+let cadena = "Esto es una cadena de texto";
+
+function nuevaCadena() {
+    let cadena = "Esto es otra cadena de texto distinta dentro de una función";
+    console.log(cadena);
+}
+
+nuevaCadena();
+console.log(cadena);
